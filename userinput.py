0560cca8cce61a0f88ae642bc2539a875a7907d4
k-bridget <k.bridget@alustudent.com>
2025-02-26 12:49:14 +0200

    python

---
+++ userinput.py
@@ -0,0 +1,11 @@
+def add_numbers():
+    a=int(input("insert the value of a "))
+    b=int(input("insert the value of b "))
+    print(a+b)
+add_numbers()
+
+#!/usr/bin/env python3
+
+name="Bridget"
+print("Hello, World")
+print("Hello" ,name)
